07cb65533c96daef81496bf591845d2c48cf2d6c
serraSH <41605080+serraSH@users.noreply.github.com>
2023-06-03 18:51:25 -0300

    Add files via upload

---
+++ build/src/config/config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    DB: {
+        URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
+        USER: '',
+        PASSWORD: ''
+    }
+};
