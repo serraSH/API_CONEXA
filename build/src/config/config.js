f1c33b34f388159b9bccb844d298e910b327d790
serraSH <41605080+serraSH@users.noreply.github.com>
2023-10-25 15:22:56 -0300

    Delete build directory

--- build/src/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    DB: {
-        URI: process.env.MONGODB_URI || 'mongodb://localhost:27017',
-        USER: '',
-        PASSWORD: ''
-    }
-};
